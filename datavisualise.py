ab399fb2da2ceddb0e318ff30b4d626f0eb2b754
fanaticjo <biswajit.mohapatra65@gmail.com>
2018-04-18 11:16:28 +0530

    Add files via upload

--- datavisualise.py
+++ datavisualise.py
@@ -1,3 +1,11 @@
+"""This file is part of GARDUINO.GARDUINO is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+as published by the Free Software Foundation, either version 3 of the License,or(at your option) any later version . 
+GARDUINO is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+See the GNU General Public License for more details.You should have received a copy of the GNU General Public License along with GARDUINO.
+If not, see <http://www.gnu.org/licenses/>. """
+
+
 from urllib.request import Request, urlopen   #rest api 
 from itertools import cycle   #infinite cycle
 import re
